2244edaae055f348282ea982c08de92778da0c87
Anbossssss <bossan321@gmail.com>
2025-08-05 22:56:32 +0700

    Update

---
+++ Classic_Pong/Classic_Pong/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Classic_Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            player1 = new PictureBox();
+            player2 = new PictureBox();
+            Ball = new PictureBox();
+            GameTimer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)player1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)player2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Ball).BeginInit();
+            SuspendLayout();
+            // 
+            // player1
+            // 
+            player1.Image = Properties.Resources.player;
+            player1.Location = new Point(31, 116);
+            player1.Margin = new Padding(3, 2, 3, 2);
+            player1.Name = "player1";
+            player1.Size = new Size(26, 90);
+            player1.SizeMode = PictureBoxSizeMode.StretchImage;
+            player1.TabIndex = 0;
+            player1.TabStop = false;
+            // 
+            // player2
+            // 
+            player2.Image = (Image)resources.GetObject("player2.Image");
+            player2.Location = new Point(632, 116);
+            player2.Margin = new Padding(3, 2, 3, 2);
+            player2.Name = "player2";
+            player2.Size = new Size(26, 90);
+            player2.SizeMode = PictureBoxSizeMode.StretchImage;
+            player2.TabIndex = 1;
+            player2.TabStop = false;
+            player2.Click += player2_Click;
+            // 
+            // Ball
+            // 
+            Ball.Image = Properties.Resources.ball;
+            Ball.Location = new Point(336, 145);
+            Ball.Margin = new Padding(3, 2, 3, 2);
+            Ball.Name = "Ball";
+            Ball.Size = new Size(26, 22);
+            Ball.SizeMode = PictureBoxSizeMode.StretchImage;
+            Ball.TabIndex = 2;
+            Ball.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            GameTimer.Enabled = true;
+            GameTimer.Interval = 20;
+            GameTimer.Tick += GameTimerEvent;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Highlight;
+            ClientSize = new Size(700, 338);
+            Controls.Add(Ball);
+            Controls.Add(player2);
+            Controls.Add(player1);
+            DoubleBuffered = true;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Player1: 0 -- Player2: 0";
+            Load += Form1_Load;
+            KeyDown += KeyIsDown;
+            KeyUp += KeyIsUp;
+            ((System.ComponentModel.ISupportInitialize)player1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)player2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Ball).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox player1;
+        private PictureBox player2;
+        private PictureBox Ball;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
---
+++ Classic_Pong/Classic_Pong/Form1.cs
@@ -0,0 +1,169 @@
+﻿namespace Classic_Pong
+{
+    public partial class Form1 : Form
+    {
+        int BallXspeed = 4;
+        int BallYspeed = 4;
+        int player1Score = 0;
+        int player2Score = 0;
+        int playerSpeed = 8;
+        Random rand = new Random();
+
+        // Trạng thái phím
+        bool goDown, goUp;
+        bool p2GoDown, p2GoUp;
+
+        int[] j = { 10, 9, 8, 11, 12 };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            Ball.Top -= BallYspeed;
+            Ball.Left -= BallXspeed;
+
+            this.Text = "Player 1: " + player1Score + " -- Player 2: " + player2Score;
+
+            // Bóng chạm trên/dưới
+            if (Ball.Top < 0 || Ball.Bottom > this.ClientSize.Height)
+            {
+                BallYspeed = -BallYspeed;
+            }
+
+            // Player 2 ghi bàn (bóng ra bên trái)
+            if (Ball.Left < -2)
+            {
+                Ball.Left = 300;
+                BallXspeed = -BallXspeed;
+                player2Score++;
+            }
+
+            // Player 1 ghi bàn (bóng ra bên phải)
+            if (Ball.Right > this.ClientSize.Width + 2)
+            {
+                Ball.Left = 300;
+                BallXspeed = -BallXspeed;
+                player1Score++;
+            }
+
+            // Giới hạn player 2 không vượt ra ngoài
+            if (player2.Top <= 0)
+            {
+                player2.Top = 0;
+            }
+            else if (player2.Bottom >= this.ClientSize.Height)
+            {
+                player2.Top = this.ClientSize.Height - player2.Height;
+            }
+
+            // Player 1 điều khiển
+            if (goDown && player1.Top + player1.Height < this.ClientSize.Height)
+            {
+                player1.Top += playerSpeed;
+            }
+            if (goUp && player1.Top > 0)
+            {
+                player1.Top -= playerSpeed;
+            }
+
+            // Player 2 điều khiển
+            if (p2GoDown && player2.Top + player2.Height < this.ClientSize.Height)
+            {
+                player2.Top += playerSpeed;
+            }
+            if (p2GoUp && player2.Top > 0)
+            {
+                player2.Top -= playerSpeed;
+            }
+
+            // Kiểm tra va chạm
+            CheckCollision(Ball, player1, player1.Right + 5);
+            CheckCollision(Ball, player2, player2.Left - 35);
+
+            // Kết thúc game
+            if (player2Score > 5)
+            {
+                GameOver("Player 2 wins!");
+            }
+            else if (player1Score > 5)
+            {
+                GameOver("Player 1 wins!");
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            // Player 1
+            if (e.KeyCode == Keys.Down)
+                goDown = true;
+            if (e.KeyCode == Keys.Up)
+                goUp = true;
+
+            // Player 2
+            if (e.KeyCode == Keys.S)
+                p2GoDown = true;
+            if (e.KeyCode == Keys.W)
+                p2GoUp = true;
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            // Player 1
+            if (e.KeyCode == Keys.Down)
+                goDown = false;
+            if (e.KeyCode == Keys.Up)
+                goUp = false;
+
+            // Player 2
+            if (e.KeyCode == Keys.S)
+                p2GoDown = false;
+            if (e.KeyCode == Keys.W)
+                p2GoUp = false;
+        }
+
+        private void CheckCollision(PictureBox PicOne, PictureBox PicTwo, int offset)
+        {
+            if (PicOne.Bounds.IntersectsWith(PicTwo.Bounds))
+            {
+                PicOne.Left = offset;
+                int x = j[rand.Next(j.Length)];
+                int y = j[rand.Next(j.Length)];
+
+                if (BallXspeed < 0)
+                    BallXspeed = x;
+                else
+                    BallXspeed = -x;
+
+                if (BallYspeed < 0)
+                    BallYspeed = -y;
+                else
+                    BallYspeed = y;
+            }
+        }
+
+        private void GameOver(string message)
+        {
+            GameTimer.Stop();
+            MessageBox.Show(message, "Game Over");
+            player2Score = 0;
+            player1Score = 0;
+            BallXspeed = BallYspeed = 4;
+            Ball.Left = 300;
+            Ball.Top = 200;
+            GameTimer.Start();
+        }
+
+        private void player2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
